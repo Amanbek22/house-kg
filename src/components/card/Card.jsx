09aae615cf36560ec8887811b844c66b79db8625
Amanbek Asylbekov <Amanbek_Asylbekov@epam.com>
2022-03-12 17:56:37 +0600

    add cards and learn hooks useState, useEffect

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,9 +1,20 @@
 import css from "./Card.module.css";
 
-export default function Card() {
+export default function Card(props) {
   return (
     <div className={css.wrapper}>
-      card
+      <div className={css.imgWrapper}>
+        <img src={props.imgUrl} alt="#" />
+      </div>
+      <br />
+      <h2>{props.price}</h2>
+      <h3>{props.title}</h3>
+      <br />
+      <button>
+        Подробнее
+      </button>
+      <br />
+      <br />
     </div>
   )
 }
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,15 +1,61 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import Card from '../components/card/Card.jsx'
 
 export default function Homepage() {
+  const [num, setNum] = useState(0);
+
+  const [houses, setHouses] = useState([]);
+
+  console.log(houses);
+
+  useEffect(() => {
+    console.log("useEffect");
+    setHouses([
+      {
+        id: 1,
+        title: "Продаю дом 4к",
+        imgUrl: "https://www.bankrate.com/2019/01/24124808/How-to-buy-a-home.jpg",
+        price: "40000$"
+      },
+      {
+        id: 2,
+        title: "Продаю кв 1к 105с",
+        imgUrl: "https://www.bankrate.com/2019/01/24124808/How-to-buy-a-home.jpg",
+        price: "30000$"
+      },
+      {
+        id: 3,
+        title: "Продаю собаку  имеется 4ноги ",
+        imgUrl: "https://www.bankrate.com/2019/01/24124808/How-to-buy-a-home.jpg",
+        price: "1000$"
+      },
+    ])
+  }, []);
+
   return (
     <div className='container page'>
-      <div className='title'>Homepage</div>
 
+
+      <div>
+        <button onClick={() => {
+          setNum(num - 1)
+        }}>Minus</button>
+        <h1>{num}</h1>
+        <button onClick={() => {
+          setNum(num + 1)
+        }}>Plus</button>
+      </div>
+
+      <div className='title'>Homepage</div>
       <div className="cardsWrapper">
-        <Card />
-        <Card />
-        <Card />
+        {
+          houses.map((house) => <Card
+            key={house.id}
+            title={house.title}
+            imgUrl={house.imgUrl}
+            price={house.price}
+          />)
+        }
       </div>
 
     </div>
